800da7dcd46f871d5b93825fcc3c14830b7781c2
Chathura Wijesinghe <cdanasiri@gmail.com>
2017-11-14 16:52:16 +0530

    Added Bank model

---
+++ app/src/main/java/lk/lian/chequeapp/cheques/model/Bank.java
@@ -0,0 +1,48 @@
+package lk.lian.chequeapp.cheques.model;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Created by Chathura Wijesinghe <cdanasiri@gmail.com> on 11/14/17.
+ */
+
+public class Bank {
+  @NonNull private int mBankCode;
+  @NonNull private String mSwiftCode;
+  @NonNull private String mBankName;
+
+  /**
+   * @param mBankCode   Bank code
+   * @param mSwiftCode Bank swift code
+   * @param mBankName Bank name
+   */
+  public Bank(@NonNull int mBankCode, @NonNull String mSwiftCode, @NonNull String mBankName) {
+    this.mBankCode = mBankCode;
+    this.mSwiftCode = mSwiftCode;
+    this.mBankName = mBankName;
+  }
+
+  @NonNull public int getmBankCode() {
+    return mBankCode;
+  }
+
+  public void setmBankCode(@NonNull int mBankCode) {
+    this.mBankCode = mBankCode;
+  }
+
+  @NonNull public String getmSwiftCode() {
+    return mSwiftCode;
+  }
+
+  public void setmSwiftCode(@NonNull String mSwiftCode) {
+    this.mSwiftCode = mSwiftCode;
+  }
+
+  @NonNull public String getmBankName() {
+    return mBankName;
+  }
+
+  public void setmBankName(@NonNull String mBankName) {
+    this.mBankName = mBankName;
+  }
+}
